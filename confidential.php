1865ab9ad4c2e8a6ce7565e1511c493962c3810e
Anup Pokharel <anup.pokharel30@gmail.com>
2022-07-22 13:25:16 +0545

    changed the db name

--- confidential.php
+++ confidential.php
@@ -1,6 +1,6 @@
 <?php
     define('DB_HOST', '127.0.0.1');
-    define('DB_NAME', 'db_forphp');
+    define('DB_NAME', 'db_books');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');  
 ?>
